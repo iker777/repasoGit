94383270abf6d83c556a3324b8a01caaae41d90b
CruzCorrales <cruzcorralesramos@gmail.com>
2022-09-27 11:32:55 +0200

    otro cambio

--- js/script.js
+++ js/script.js
@@ -9,5 +9,10 @@ function cambiarColor (){
 function cambiarPosicion (){
     imagen3.style.order = -1;
 }
+function volverAtras (){
+    cruz.style.backgroundColor = "white";
+    imagen3.style.order = 0;
+}
 imagen1.addEventListener("click", cambiarColor);
 imagen2.addEventListener("click", cambiarPosicion);
+imagen3.addEventListener("click", volverAtras);
